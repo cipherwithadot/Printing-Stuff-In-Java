5ae4035639316d17220db8a4d2628f8ab35771ae
Cipher <88819188+Cipher-GF@users.noreply.github.com>
2022-06-01 18:56:25 +0530

    :C

---
+++ Printing.java
@@ -0,0 +1,14 @@
+public class Printing {
+    
+    public static void main (String args[]){
+    
+    String name = "cipher";
+	
+    int a = 5;
+    int b = 2;
+    int c = a + b;
+	
+    System.out.println("cipher is " + c + " years old :O");
+    
+    }
+}
